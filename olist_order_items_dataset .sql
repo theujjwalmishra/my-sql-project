5d2571a4cde9377fa5f10b3f7d67ec86d1d58e7c
Ujjwal Mishra <ujjwalmishra.cse2024@agra.sharda.ac.in>
2026-08-21 15:55:39 +0530

    Update olist_order_items_dataset .sql
    add this

--- olist_order_items_dataset .sql
+++ olist_order_items_dataset .sql
@@ -1,7 +1,35 @@
--- drop database if exists sqlproject;
--- create database sqlproject;
--- use sqlproject;
+drop database if exists sqlproject;
+create database sqlproject;
+use sqlproject;
 
--- show tables;
+show tables;
 select * from olist_order_items_dataset;
 
+-- Total Orders
+select Count( distinct(order_id) ) order_item_id 
+from olist_order_items_dataset;
+
+-- Total Revenues
+Select SUM(price) AS Total_Revenues
+From olist_order_items_dataset;
+
+-- Average product price
+Select Avg (price) as Average 
+From olist_order_items_dataset;
+
+-- Top 5 sabse mehenga products
+Select price 
+From olist_order_items_dataset
+Order By price DESC
+LIMIT 5;
+
+-- Top 10 sellers in terms of revenue
+Select seller_id 
+From olist_order_items_dataset
+Group by seller_id
+Order by price DESC
+LIMIT 10;
+
+-- Total shipping cost kitna pada?
+Select SUM(freight_value) AS Total_shipping_cost
+from olist_order_items_dataset;
